1301bde059ac75079bb3785bb31088c60d12e185
GeertVanEspen <geert@vanespen.com>
2026-04-13 07:41:54 +0200

    Update camDashboard_config_private.py

--- camDashboard_config_private.py
+++ camDashboard_config_private.py
@@ -7,14 +7,14 @@ DIT BESTAND MAG NOOIT IN GITHUB KOMEN!
 # ===================== GEVOELIGE CONFIG =====================
 
 # RTSP stream (bevat wachtwoord)
-RTSP_URL = "rtsp://admin:Whoo1124@10.42.0.182:554/h264Preview_01_sub"
+RTSP_URL = "rtsp://admin:password@10.42.0.182:554/h264Preview_01_sub"
 
 # Basic Auth voor de upload naar je webserver
-USERNAME = "geert"
-PASSWORD = "Brompt1124"
+USERNAME = "username"
+PASSWORD = "password"
 
 # rclone remote (OneDrive)
-CLOUD_REMOTE = "onedrive:/cam/ReoMPA"
+CLOUD_REMOTE = "onedrive:/cam/mycamname"
 
 # Optioneel: andere gevoelige dingen die je later eventueel wilt toevoegen
 # UPLOAD_URL = "http://geert.zapto.org/..."   # als je deze ook privé wilt houden
